1d17286185a484ec89cfbc6deddcb9fd71b677a0
Todd Moody <todd@moodsdigital.co.nz>
2018-03-06 16:04:29 +1300

    add baseline contact form and integration to formspree

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react';
 class ContactForm extends Component {
     render() {
       return (
-        <div id="contact-form" className="bg-alt">
-          <p>Contact form Section</p>
+        <div id="contact-form">
+            <form action="https://formspree.io/todd@moodsdigital.co.nz" method="POST">
+              <div>
+                <input className="" type="text" id="contact-name" name="contact-name" placeholder="name" required />
+                <input className="" type="email" id="contact-email" name="contact-email" placeholder="email address" required />
+              </div>
+              <div>
+                <input className="" type="text" id="contact-phone" name="contact-phone" placeholder="phone" />
+                <input className="" type="text" id="contact-subject" name="contact-subject" placeholder="subject" required />
+              </div>
+              <div>
+                <textarea className="" id="contact-message" name="contact-message" placeholder="Your message" />
+              </div>
+              <button className="" type="submit" id="contact-submit" name="contact-submit" value="submit">Send Message</button>
+          </form>
         </div>
       );
     }
