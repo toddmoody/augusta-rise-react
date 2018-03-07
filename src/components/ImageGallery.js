80ebbf7af60bdd9eb69c5917e1177115761b1eb2
Todd Moody <todd@moodsdigital.co.nz>
2018-03-07 16:15:55 +1300

    base gallery component added

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -5,8 +5,11 @@ import './css/ImageGallery.css';
 class ImageGallery extends Component {
     render() {
       return (
-        <div>
-          <p>Image Gallery!</p>
+        <div id="content">
+          <div className="one">1</div>
+          <div className="two">2</div>
+          <div className="three">3</div>
+          <div className="four">4</div>
         </div>
       );
     }
