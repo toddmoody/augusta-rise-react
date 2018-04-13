c679cec140e0e319691a5b5bea35dd572957b0f1
Todd Moody <todd@moodsdigital.co.nz>
2018-04-13 14:58:07 +1200

    add new listings and revise gallery flex styles

--- src/App.js
+++ src/App.js
@@ -12,6 +12,51 @@ import ContactDets from './components/ContactDets'
 import ContactForm from './components/ContactForm'
 import Footer from './components/Footer'
 
+const listingDets = [
+  {
+    listing: "Lot 1",
+    price: "$669000",
+    status: "For sale",
+    button: "More Details",
+    url: "https://barretthomes.co.nz/property/lot-1-augusta-rise-omokoroa/"
+  },
+  {
+    listing: "Lot 10",
+    price: "$669000",
+    status: "For sale",
+    button: "More Details",
+    url: "https://barretthomes.co.nz/property/lot-10-augusta-rise-omokoroa/"
+  },
+  {
+    listing: "Lot 39",
+    price: "$669000",
+    status: "For sale",
+    button: "More Details",
+    url: "https://barretthomes.co.nz/property/lot-39-augusta-rise-omokoroa/"
+  },
+  {
+    listing: "Lot 3",
+    price: "$669000",
+    status: "Under Offer",
+    button: "More Details",
+    url: "https://barretthomes.co.nz/property/lot-3-augusta-rise-omokoroa/"
+  },
+  {
+    listing: "Lot 40",
+    price: "$669000",
+    status: "Under Offer",
+    button: "More Details",
+    url: "https://barretthomes.co.nz/property/lot-40-augusta-rise-omokoroa/"
+  },
+  {
+    listing: "Lot 42",
+    price: "$669000",
+    status: "Under Offer",
+    button: "More Details",
+    url: "https://barretthomes.co.nz/property/lot-42-augusta-rise-omokoroa/"
+  }
+]
+
 class App extends Component {
   render() {
     return (
@@ -20,7 +65,7 @@ class App extends Component {
       <Intro />
       <ParallaxRender />
       <StageOne />
-      <ImageGallery />
+      <ImageGallery data={listingDets} />
       <SpeakToday />
       <NotReady />
       <ContactDets />
--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -3,77 +3,64 @@ import React from 'react';
 import './css/ImageGallery.css';
 
 import LotOne from '../assets/gallery/lot-1-gall.jpg'
+import LotTen from '../assets/gallery/lot-10-gall.jpg'
+import LotThirtyNine from '../assets/gallery/lot-39-gall.jpg' 
 import LotThree from '../assets/gallery/lot-3-gall.jpg'
 import LotFourty from '../assets/gallery/lot-40-gall.jpg'
 import LotFourtyTwo from '../assets/gallery/lot-42-gall.jpg'
-
-const listingDets = [
-  {
-    listing: "Lot 1",
-    price: "$669000",
-    status: "For sale",
-    button: "More Details",
-    url: "https://barretthomes.co.nz/property/lot-1-augusta-rise-omokoroa/"
-  },
-  {
-    listing: "Lot 3",
-    price: "$669000",
-    status: "Under Offer",
-    button: "More Details",
-    url: "https://barretthomes.co.nz/property/lot-3-augusta-rise-omokoroa/"
-  },
-  {
-    listing: "Lot 40",
-    price: "$669000",
-    status: "Under Offer",
-    button: "More Details",
-    url: "https://barretthomes.co.nz/property/lot-40-augusta-rise-omokoroa/"
-  },
-  {
-    listing: "Lot 42",
-    price: "$669000",
-    status: "Under Offer",
-    button: "More Details",
-    url: "https://barretthomes.co.nz/property/lot-42-augusta-rise-omokoroa/"
-  }
-]
 
 class ImageGallery extends React.Component {
 
   render() {
    
     return (
-        <section className="flex-container">
-         <div className="image-container">
-          <img className="image" src={LotOne} alt="Lot 1 - Augusta Rise" />
-            <div className="tag">{listingDets[0].status}</div>
-          <div className="middle">
-            <h3>{listingDets[0].listing}</h3>
-            <a href={listingDets[0].url} target="blank"><button>{listingDets[0].button}</button></a>
-          </div>
+    <section className="flex-container">
+      <div className="image-container">
+      <img className="image" src={LotOne} alt="Lot 1 - Augusta Rise" />
+        <div className="tag">{this.props.data[0].status}</div>
+        <div className="middle">
+          <h3>{this.props.data[0].listing}</h3>
+          <a href={this.props.data[0].url} target="blank"><button>{this.props.data[0].button}</button></a>
         </div>
+      </div>
+      <div className="image-container">
+        <img className="image" src={LotTen} alt="Lot 10 - Augusta Rise" />
+        <div className="tag">{this.props.data[1].status}</div>
+      <div className="middle">
+        <h3>{this.props.data[1].listing}</h3>
+        <a href={this.props.data[1].url} target="blank"><button>{this.props.data[1].button}</button></a>
+      </div>
+    </div>
+    <div className="image-container">
+        <img className="image" src={LotThirtyNine} alt="Lot 39 - Augusta Rise" />
+        <div className="tag">{this.props.data[2].status}</div>
+      <div className="middle">
+        <h3>{this.props.data[2].listing}</h3>
+        <a href={this.props.data[2].url} target="blank"><button>{this.props.data[2].button}</button></a>
+      </div>
+    </div>
+    <div className="image-container">
+        <img className="image" src={LotThree} alt="Lot 3 - Augusta Rise" />
+        <div className="tag">{this.props.data[3].status}</div>
+      <div className="middle">
+        <h3>{this.props.data[3].listing}</h3>
+        <a href={this.props.data[3].url} target="blank"><button>{this.props.data[3].button}</button></a>
+      </div>
+    </div>
+    <div className="image-container">
+      <img className="image" src={LotFourty} alt="Lot 40 - Augusta Rise" />
+      <div className="tag">{this.props.data[4].status}</div>
+      <div className="middle">
+        <h3>{this.props.data[4].listing}</h3>
+        <a href={this.props.data[4].url} target="blank"><button>{this.props.data[4].button}</button></a>
+      </div>
+    </div>
         <div className="image-container">
-          <img className="image" src={LotThree} alt="Lot 1 - Augusta Rise" />
-            <div className="tag">{listingDets[1].status}</div>
+          <img className="image" src={LotFourtyTwo} alt="Lot 42 - Augusta Rise" />
+          <div className="tag">{this.props.data[5].status}</div>
           <div className="middle">
-            <h3>{listingDets[1].listing}</h3>
-            <a href={listingDets[1].url} target="blank"><button>{listingDets[1].button}</button></a>
-          </div>
-        </div>
-        <div className="image-container">
-          <img className="image" src={LotFourty} alt="Lot 1 - Augusta Rise" />
-            <div className="tag">{listingDets[2].status}</div>
-          <div className="middle">
-            <h3>{listingDets[2].listing}</h3>
-            <a href={listingDets[2].url} target="blank"><button>{listingDets[2].button}</button></a>
-          </div>
-        </div>
-        <div className="image-container">
-          <img className="image" src={LotFourtyTwo} alt="Lot 1 - Augusta Rise" />
-            <div className="tag">{listingDets[3].status}</div>
-          <div className="middle">
-            <h3>{listingDets[3].listing}</h3>
-            <a href={listingDets[3].url} target="blank"><button>{listingDets[3].button}</button></a>
+            <h3>{this.props.data[5].listing}</h3>
+            <a href={this.props.data[5].url} target="blank"><button>{this.props.data[5].button}</button></a>
           </div>
         </div>
       </section>
